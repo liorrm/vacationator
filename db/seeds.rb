166c4872570b17fb284d0830e87536b4bc8b4c87
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2014-12-05 14:10:42 -0800

    added rakefile and some more needed features to app

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+
+
+name
+season
+cost
+activity
+description
+
+Destination.create(name: "Lake Tahoe, CA", season: "winter", cost: "$$$", activity: "outdoors" description: "You love skiing, we have snow, so come to Tahoe")
+
+Destination.create(name: "Bogotá, Colombia", season: "spring", cost: "$$", activity: "" description: "Ven a la ciudad más linda en toda sudamérica")
+
+Destination.create(name: "Los Angeles, CA", season: "winter", cost: "$$", activity: "" description: "Whether you want plastic surgery or see a bunch of people who have it, Los Angeles has got you covered")
+
+Destination.create(name: "Hot Springs, AR", season: "summer", cost: "$$", activity: "" description: "Ven a la ciudad más linda en toda sudamérica")
+
+Destination.create(name: "Hot Springs, AR", season: "fall", cost: "$", activity: "" description: "We have good music")
