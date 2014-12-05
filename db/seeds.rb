6c8483a7e0b7535096f05fd1c04bbfbc2354481e
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2014-12-05 14:35:41 -0800

    added gems to gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
+require_relative ''
 
-
-name
-season
-cost
-activity
-description
+# name
+# season
+# cost
+# activity
+# description
 
 Destination.create(name: "Lake Tahoe, CA", season: "winter", cost: "$$$", activity: "outdoors" description: "You love skiing, we have snow, so come to Tahoe")
 
-Destination.create(name: "Bogotá, Colombia", season: "spring", cost: "$$", activity: "" description: "Ven a la ciudad más linda en toda sudamérica")
+Destination.create(name: "Cartagena, Colombia", season: "spring", cost: "$$", activity: "" description: "Ven a la ciudad más linda en toda sudamérica")
 
 Destination.create(name: "Los Angeles, CA", season: "winter", cost: "$$", activity: "" description: "Whether you want plastic surgery or see a bunch of people who have it, Los Angeles has got you covered")
 
